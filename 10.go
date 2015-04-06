7826b05103fa45638bfbb21f7a9a5705a740bd0e
v.tikhonov <vladimir@tikhonov.by>
2015-04-06 18:04:05 +0300

    Task #10

---
+++ 10.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var sum int64 = 2
+	for i := 3; i < 2000000; i += 2 {
+		if isPrime(i) {
+			sum += int64(i)
+		}
+	}
+	fmt.Println(sum)
+}
+
+func isPrime(value int) bool {
+	if value%3 == 0 || value%5 == 0 || value%7 == 0 || value%11 == 0 {
+		return false
+	}
+	for i := 3; i < value/2+1; i += 2 {
+		if value%i == 0 {
+			return false
+		}
+	}
+	return true
+}
